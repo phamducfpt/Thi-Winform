d2492a4220ebe125366fefe3bd3b57ae1c68e2f5
phamducfpt <ducpmth2009023@fpt.edu.vn>
2021-11-26 10:32:21 +0700

    phamduc

---
+++ Windows_Form/Pages/NavigationView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Windows_Form.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class NavigationView : Page
+    {
+        public NavigationView()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private void NavigationView_SelectionChanged(Windows.UI.Xaml.Controls.NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (sender is null)
+            {
+                throw new ArgumentNullException(nameof(sender));
+            }
+
+            if (args.IsSettingsSelected)
+            {
+                //click nut' setting
+                Debug.WriteLine("select setting");
+            }
+            var navigationViewItem = args.SelectedItem as NavigationViewItem;
+            switch (navigationViewItem.Name)
+            {
+                case "add":
+                    this.contentFrame1.Navigate(typeof(Pages.AddContact));
+                    break;
+                case "search":
+                    this.contentFrame1.Navigate(typeof(Pages.SearchContact));
+                    break;
+            }
+        }
+    }
+}
